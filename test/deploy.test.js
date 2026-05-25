2a2c8366549d74c589b6d6a487e8576d741c641a
Robin <robinvdveer@gmail.com>
2026-05-25 17:11:57 +0200

    Fix Keycloak public URLs and remove bundled credentials

--- test/deploy.test.js
+++ test/deploy.test.js
@@ -71,7 +71,7 @@ describe('helm chart deployability conventions', () => {
 
     expect(appDeployment.spec.template.spec.containers[0].image).toBe('example.test/app:abc123');
     expect(appDeployment.spec.template.spec.containers[0].env).toEqual(expect.arrayContaining([
-      expect.objectContaining({ name: 'KEYCLOAK_ISSUER_URL', value: 'http://localhost:8081/realms/todos' }),
+      expect.objectContaining({ name: 'KEYCLOAK_ISSUER_URL', value: 'http://keycloak.metrics-server.local/realms/todos' }),
       expect.objectContaining({ name: 'KEYCLOAK_JWKS_URL' }),
       expect.objectContaining({ name: 'KEYCLOAK_CLIENT_ID', value: 'todo-app' }),
     ]));
